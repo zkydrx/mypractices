c3cd41bf86792c999b30fca95775738649b6844d
zky <1964483239@qq.com>
2017-08-20 10:57:22 +0800

    这个方法不是我想到的，是我摘自网上。很巧妙。

--- src/wanMeiPingFang/PerfectPow.java
+++ src/wanMeiPingFang/PerfectPow.java
@@ -1,37 +1,51 @@
 package wanMeiPingFang;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by ZKY on 2017-08-20 01:06.
- *
- *
+ * <p>
+ * <p>
  * 给一个正整数 n, 找到若干个完全平方数(比如1, 4, 9, ... )使得他们的和等于 n。你需要让平方数的个数最少。
- *样例
- 给出 n = 12, 返回 3 因为 12 = 4 + 4 + 4。
- 给出 n = 13, 返回 2 因为 13 = 4 + 9。
- *
+ * 样例
+ * 给出 n = 12, 返回 3 因为 12 = 4 + 4 + 4。
+ * 给出 n = 13, 返回 2 因为 13 = 4 + 9。
  */
 public class PerfectPow
 {
+
     /**
-     * 获取某数所有平方数，此数值小于或等于给定的正整数的平方根
+     * 把每一个数换成是平方数和的个数。
      * @param n
      * @return
      */
-    public List<Long> getIntegerList(int n)
+    public int numSquares(int n)
     {
-        List<Long> list = new ArrayList<>();
-
-        for(int i = 1; i <= n; i++)
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n + 1; ++i)
         {
-            list.add((long)Math.pow(i,2));
+            dp[i] = i;
         }
-        return list;
+        for (int i = 0; i < n + 1; ++i)
+        {
+            for (int j = 2; j <= Math.sqrt(i); ++j)
+            {
+                if (j * j == i)
+                {
+                    dp[i] = 1;
+                    break;
+                }
+                /**
+                 * 所有的精髓都在这个表达式中。简约，精巧。
+                 */
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
     }
 
-
-
-
+    public static void main(String[] args)
+    {
+        PerfectPow perfectPow = new PerfectPow();
+        int i = perfectPow.numSquares(10);
+        System.out.println(i);
+    }
 }
