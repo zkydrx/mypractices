bcdc7492905b0bf59d8c8bfed8b3d9908c36fab7
zky <1964483239@qq.com>
2017-08-22 01:16:35 +0800

    add I > 3 Java

---
+++ src/fromeFaceBook/I3Java/Domain.java
@@ -0,0 +1,8 @@
+package fromeFaceBook.I3Java;
+
+/**
+ * Created by ZKY on 2017-08-27 01:49.
+ */
+public class Domain
+{
+}
---
+++ src/fromeFaceBook/I3Java/Test.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by ZKY on 2017-08-27 02:04.
+ */
+class Test
+{
+
+}
