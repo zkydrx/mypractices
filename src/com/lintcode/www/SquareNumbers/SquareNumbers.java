9a568236fd73caf98429d71be0a865125aa34e78
zky <1964483239@qq.com>
2017-10-03 19:28:48 +0800

    optimization algorithm.

--- src/com/lintcode/www/SquareNumbers/SquareNumbers.java
+++ src/com/lintcode/www/SquareNumbers/SquareNumbers.java
@@ -16,25 +16,73 @@ package com.lintcode.www.SquareNumbers;
  */
 public class SquareNumbers
 {
+    public static void main(String[] args)
+    {
+        SquareNumbers squareNumbers = new SquareNumbers();
+        boolean b = squareNumbers.checkSumOfSquareNumbers(5);
+        System.out.println(b);
+        System.out.println(Math.sqrt(4));
+    }
+
+    /**
+     * 方法一：时间复杂度较方法二小。
+     * @param num
+     * @return
+     */
     public boolean checkSumOfSquareNumbers(int num)
     {
+        /**
+         * 这个方法的时间复杂度比刚才那个小很多。
+         */
+        for (int i = 0; i <= Math.sqrt(num); i++)
+        {
+            int tem = num - i * i;
+            if (checkTheNumber(tem))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Judge the number is a square number or not.
+     *
+     * @param num
+     * @return
+     */
+    public boolean checkTheNumber(int num)
+    {
+        for (int i = 0; i <= Math.sqrt(num); i++)
+        {
+            if (num == i * i)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 方法二：时间复杂度较大。
+     * @param num
+     * @return
+     */
+    public boolean checkSumOfSquareNumbers1(int num)
+    {
+        /**
+         * 时间复杂度太大。
+         */
         for (int i = 0; i <= Math.sqrt(num); i++)
         {
             for (int j = 0; j <= Math.sqrt(num); j++)
             {
-                if(num == Math.pow(i,2)+Math.pow(j,2))
+                if (num == Math.pow(i, 2) + Math.pow(j, 2))
                 {
                     return true;
                 }
             }
         }
         return false;
-    }
-
-    public static void main(String[] args)
-    {
-        SquareNumbers squareNumbers = new SquareNumbers();
-        boolean b = squareNumbers.checkSumOfSquareNumbers(5);
-        System.out.println(b);
     }
 }
