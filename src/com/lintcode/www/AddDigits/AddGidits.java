3d9ef034cdeb2f95cbe41a34664a7b70429b6991
zky <1964483239@qq.com>
2017-10-05 23:40:48 +0800

    Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.

---
+++ src/com/lintcode/www/AddDigits/AddGidits.java
@@ -0,0 +1,20 @@
+package com.lintcode.www.AddDigits;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-10-05
+ * Time: 23:39
+ * Description:
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ * Example
+ * Given num = 38.
+ * The process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return 2.
+ */
+public class AddGidits
+{
+    public int addDigits(int num)
+    {
+        // write your code here
+    }
+}
