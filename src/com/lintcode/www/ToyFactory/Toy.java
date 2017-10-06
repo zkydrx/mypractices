777ef06fb13ddef0c0353b58c4df09bb033c973d
zky <1964483239@qq.com>
2017-10-07 00:48:48 +0800

    Factory is a design pattern in common usage. Please implement a ToyFactory which can generate proper toy based on the given type.

---
+++ src/com/lintcode/www/ToyFactory/Toy.java
@@ -0,0 +1,13 @@
+package com.lintcode.www.ToyFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-10-07
+ * Time: 00:44
+ * Description:
+ */
+public interface Toy
+{
+    void talk();
+}
---
+++ src/com/lintcode/www/ToyFactory/ToyFactory.java
@@ -0,0 +1,17 @@
+package com.lintcode.www.ToyFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-10-07
+ * Time: 00:46
+ * Description:
+ */
+public class ToyFactory
+{
+
+    /*public Toy getToy(String type)
+    {
+
+    */}
+}
