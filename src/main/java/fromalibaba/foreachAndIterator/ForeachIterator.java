53a3b9226997f277082a0ec3b564d11b6e90c1ed
zky <1964483239@qq.com>
2017-11-29 13:05:18 +0800

    alibaba's develop book.

---
+++ src/main/java/fromalibaba/foreachAndIterator/ForeachIterator.java
@@ -0,0 +1,42 @@
+package fromalibaba.foreachAndIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2017-11-29
+ * Time: 12:07
+ * Description:
+ */
+public class ForeachIterator
+{
+    public static void main(String[] args)
+    {
+        List<String> list = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list1.add("1");
+        list1.add("2");
+
+        for (String s : list)
+        {
+            if("1".equals(s))
+            {
+                list.remove(s);
+            }
+        }
+        System.out.println(list);
+
+//        for (String s : list1)
+//        {
+//            if("2".equals(s))
+//            {
+//                list1.remove(s);
+//            }
+//        }
+
+    }
+}
