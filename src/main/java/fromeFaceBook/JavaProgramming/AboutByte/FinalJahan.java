0e390137c4fd58359e7d1fd627c5723c7e38334d
zky <1964483239@qq.com>
2017-12-15 08:51:59 +0800

    MAX_VALUE,MIN_VALUE

--- src/main/java/fromeFaceBook/JavaProgramming/AboutByte/FinalJahan.java
+++ src/main/java/fromeFaceBook/JavaProgramming/AboutByte/FinalJahan.java
@@ -38,5 +38,38 @@ public class FinalJahan
         {
             System.out.println("Sorry! you not prosperous");
         }
+
+
+        byte max_byte = Byte.MAX_VALUE;
+        byte min_byte = Byte.MIN_VALUE;
+        short max_short = Short.MAX_VALUE;
+        short min_short = Short.MIN_VALUE;
+        int max_int = Integer.MAX_VALUE;
+        int min_int = Integer.MIN_VALUE;
+        long max_long = Long.MAX_VALUE;
+        long min_long = Long.MIN_VALUE;
+        float max_float = Float.MAX_VALUE;
+        float min_float = Float.MIN_VALUE;
+        double max_double = Double.MAX_VALUE;
+        double min_double = Double.MIN_VALUE;
+        char max_char = Character.MAX_VALUE;
+        char min_char = Character.MIN_VALUE;
+
+        System.out.println("max_byte:"+max_byte);
+        System.out.println("max_short:"+max_short);
+        System.out.println("max_int:"+max_int);
+        System.out.println("max_long:"+max_long);
+        System.out.println("max_float:"+max_float);
+        System.out.println("max_double:"+max_double);
+        System.out.println("max_char:"+max_char);
+
+        System.out.println("min_byte:"+min_byte);
+        System.out.println("min_short:"+min_short);
+        System.out.println("min_int:"+min_int);
+        System.out.println("min_long:"+min_long);
+        System.out.println("min_float:"+min_float);
+        System.out.println("min_double:"+min_double);
+        System.out.println("min_char:"+min_char);
+
     }
 }
