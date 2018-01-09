1817fa189b79cb36bb80eff83c04341c3fd29404
zky <1964483239@qq.com>
2018-01-09 18:09:13 +0800

    258. Add Digits

---
+++ src/main/java/com/leetcode/AddDigits258/AddDigits.java
@@ -0,0 +1,56 @@
+package com.leetcode.AddDigits258;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Abbot
+ * Date: 2018-01-09
+ * Time: 17:58
+ * Description:
+ * <p>
+ * 258. Add Digits
+ * <p>
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ * <p>
+ * For example:
+ * <p>
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ * <p>
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
+ * <p>
+ * Credits:
+ * Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+ */
+public class AddDigits
+{
+    public static int getDidits(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int tem = num % 10;
+            sum += tem;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    public int addDigits(int num)
+    {
+        int tem = getDidits(num);
+        while (tem >= 10)
+        {
+            tem = getDidits(tem);
+        }
+
+        return tem;
+    }
+
+    public static void main(String[] args)
+    {
+        int i = new AddDigits().addDigits(38);
+
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/leetcode/AddDigits258/README.md
@@ -0,0 +1,4 @@
+#Exclain
+1. The subject is easy and good.
+2. Object oriented programming.
+3. To be or not to be that is a question. 
