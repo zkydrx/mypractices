fa80a23e6a5a6268aedafe24b04208c0bcd911b7
zky <1964483239@qq.com>
2018-01-18 16:47:33 +0800

    278. First Bad Version

---
+++ src/main/java/com/leetcode/FirstBadVersion278/FirstBadVersion.java
@@ -0,0 +1,58 @@
+package com.leetcode.FirstBadVersion278;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-18
+ * Time: 16:34
+ * Description:
+ *
+ * 278. First Bad Version
+ *
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+ Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+ You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ Credits:
+ Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+
+
+ */
+public class FirstBadVersion
+{
+    public int findFirstBadVersion(int n)
+    {
+
+        int min = 0;
+        int max = n;
+        int mid;
+        /**
+         *
+         */
+        while (max > min)
+        {
+            mid = min + (max - min) / 2;
+            if (isBadVersion(mid))
+            {
+                max = mid;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * this is a example.
+     * @param mid
+     * @return
+     */
+    private boolean isBadVersion(int mid)
+    {
+        return false;
+    }
+}
---
+++ src/main/java/com/leetcode/FirstBadVersion278/README.md
@@ -0,0 +1,4 @@
+# Explain
+1. The subject was realistic and meaningful
+2. The dichotomy was powerful.
+3. Keep lively and keep passion in the code.
