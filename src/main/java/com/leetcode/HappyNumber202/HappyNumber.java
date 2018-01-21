b9766502e404a6aac5e436ef7c041b0b8f298938
zky <1964483239@qq.com>
2018-01-21 23:56:11 +0800

    202. Happy Number

---
+++ src/main/java/com/leetcode/HappyNumber202/HappyNumber.java
@@ -0,0 +1,61 @@
+package com.leetcode.HappyNumber202;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-21
+ * Time: 23:28
+ * Description:
+ * 202. Happy Number
+ * Write an algorithm to determine if a number is "happy".
+ * <p>
+ * A happy number is a number defined by the following process: Starting with any positive integer, replace the
+ * number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will
+ * stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1
+ * are happy numbers.
+ * <p>
+ * Example: 19 is a happy number
+ * <p>
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * Credits:
+ * Special thanks to @mithmatt and @ts for adding this problem and creating all test cases.
+ */
+public class HappyNumber
+{
+    public static void main(String[] args)
+    {
+        HappyNumber happyNumber = new HappyNumber();
+
+        boolean happy = happyNumber.isHappy(19);
+        System.out.println(happy);
+
+    }
+
+    public boolean isHappy(int n)
+    {
+        if (n != 2 || n ==3)
+        {
+            while (n > 1)
+            {
+                n = getResultNum(n);
+            }
+        }
+
+        return n == 1;
+    }
+
+    public int getResultNum(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            int temp = n % 10;
+            sum += temp * temp;
+            n /= 10;
+        }
+        return sum;
+    }
+}
