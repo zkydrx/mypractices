f4ed3612142ee47c853325ba259bf7505c144c33
zky <1964483239@qq.com>
2018-01-23 21:34:36 +0800

    226. Invert Binary Tree

---
+++ src/main/java/com/leetcode/InvertBinaryTree226/README.md
@@ -0,0 +1,3 @@
+#Explain
+1. The subject not difficult 
+2. Coding is powerful and interesting
---
+++ src/main/java/com/leetcode/InvertBinaryTree226/TreeNode.java
@@ -0,0 +1,20 @@
+package com.leetcode.InvertBinaryTree226;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-23
+ * Time: 18:50:24
+ * Description:
+ */
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
