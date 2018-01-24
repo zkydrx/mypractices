b3b6e5b80d4a1e756b4dbe63473303461959357e
zky <1964483239@qq.com>
2018-01-24 23:59:52 +0800

    204. Count Primes

---
+++ src/main/java/com/leetcode/CountPrimes204/CountPrimes.java
@@ -0,0 +1,103 @@
+package com.leetcode.CountPrimes204;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-24
+ * Time: 23:30:34
+ * Description:
+ * 204. Count Primes
+ * <p>
+ * Description:
+ * <p>
+ * Count the number of prime numbers less than a non-negative number, n.
+ * <p>
+ * Credits:
+ * Special thanks to @mithmatt for adding this problem and creating all test cases.
+ */
+public class CountPrimes
+{
+    public static void main(String[] args)
+    {
+        int i = new CountPrimes().countPrimes(10);
+
+        int result = new CountPrimes().getResult(10);
+
+        System.out.println(result);
+        System.out.println(i);
+    }
+
+    public int countPrimes(int n)
+    {
+        int count = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (isPrime(i))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isPrime(int n)
+    {
+        boolean b = true;
+        if (n == 1)
+        {
+            b = false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                b = false;
+                break;
+            }
+        }
+        return b;
+    }
+
+
+    public int getResult(int n)
+    {
+        n = n - 1;
+
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+
+        if (n <= 1)
+            return 0;
+        if (n == 2)
+            return 1;
+        if (n == 3)
+            return 2;
+
+        primes.add(2);
+        primes.add(3);
+
+        for (int i = 4; i <= n; i++)
+        {
+            boolean isPrime = true;
+            for (int p : primes)
+            {
+                int m = i % p;
+                if (m == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                primes.add(i);
+            }
+        }
+
+        return primes.size();
+    }
+
+}
