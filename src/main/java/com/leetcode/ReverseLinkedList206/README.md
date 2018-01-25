0c2cfa5c1587dab9ffc99e9ec9309b8d8dfbcf60
zky <1964483239@qq.com>
2018-01-25 23:46:50 +0800

    206. Reverse Linked List

---
+++ src/main/java/com/leetcode/ReverseLinkedList206/README.md
@@ -0,0 +1,4 @@
+#Explain
+1. This subject not really difficult and it let me think a lot because I don't like to do with the LinkedList
+2. real programmers don't write specs—— users should consider themselves
+   lucky to get any programs at all and take what they get.
---
+++ src/main/java/com/leetcode/ReverseLinkedList206/ReverseLinkedList.java
@@ -0,0 +1,73 @@
+package com.leetcode.ReverseLinkedList206;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-25
+ * Time: 22:40:27
+ * Description:
+ * 206. Reverse Linked List
+ * <p>
+ * Reverse a singly linked list.
+ * <p>
+ * click to show more hints.
+ * <p>
+ * Hint:
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+public class ReverseLinkedList
+{
+    static ListNode head;
+
+    public static void main(String[] args)
+    {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        ListNode listNode1 = new ReverseLinkedList().reverseList(listNode);
+
+        new ReverseLinkedList().printListNode(listNode1);
+    }
+
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode prev = null;
+
+        ListNode current = head;
+
+        ListNode next = null;
+
+        while (current != null)
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+        return head;
+    }
+
+    public void printListNode(ListNode listNode)
+    {
+        while (listNode != null)
+        {
+            System.out.print(listNode.data + "\t");
+            listNode = listNode.next;
+        }
+    }
+
+    static class ListNode
+    {
+        int data;
+        ListNode next;
+
+        ListNode(int x)
+        {
+            data = x;
+            next = null;
+        }
+    }
+}
