37a3724ce95bcdf58e7e1f32ece8c1845f49a53a
zky <1964483239@qq.com>
2018-01-27 23:41:52 +0800

    191. Number of 1 Bits

---
+++ src/main/java/com/leetcode/NumberOf1Bits191/NumberOf1Bits.java
@@ -0,0 +1,51 @@
+package com.leetcode.NumberOf1Bits191;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-01-27
+ * Time: 22:42:42
+ * Description:
+ * 191. Number of 1 Bits
+ * Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the
+ * Hamming weight).
+ * <p>
+ * For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function
+ * should return 3.
+ * <p>
+ * Credits:
+ * Special thanks to @ts for adding this problem and creating all test cases.
+ */
+public class NumberOf1Bits
+{
+    public static void main(String[] args)
+    {
+        int i = new NumberOf1Bits().hammingWeight(3);
+        int i1 = new NumberOf1Bits().hammingWeightOne(3);
+
+        System.out.println(i);
+        System.out.println(i1);
+    }
+
+    public int hammingWeight(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+    public int hammingWeightOne(int n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            n &=(n-1);
+            count++;
+        }
+        return count;
+    }
+}
---
+++ src/main/java/com/leetcode/NumberOf1Bits191/README.md
@@ -0,0 +1,3 @@
+#Explain
+1. In the leetcode the answer is not right and I think it is no problem so I have no idea.
+2. I will be a good programmer and I could write good code.
