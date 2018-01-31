00a2633f398219c1a2ee164b6f0a804d99571e1e
zky <1964483239@qq.com>
2018-01-31 09:37:21 +0800

    633. Sum of Square Numbers

---
+++ src/main/java/com/leetcode/SumOfSquareNumbers633/README.md
@@ -0,0 +1,3 @@
+#Explain
+1. I write the first method and I got three way from the internet
+2. Why not get the good way by myself and it is should be think more and more.
--- src/main/java/com/leetcode/SumOfSquareNumbers633/SumOfSqareNumbers.java
+++ src/main/java/com/leetcode/SumOfSquareNumbers633/SumOfSqareNumbers.java
@@ -1,5 +1,8 @@
 package com.leetcode.SumOfSquareNumbers633;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * Author: Abbot
@@ -9,6 +12,22 @@ package com.leetcode.SumOfSquareNumbers633;
  */
 public class SumOfSqareNumbers
 {
+    public static void main(String[] args)
+    {
+        boolean b = new SumOfSqareNumbers().judgeSquareSum(4);
+
+        System.out.println(b);
+        boolean b1 = new SumOfSqareNumbers().judgeSquareSumOne(16);
+
+        System.out.println(b1);
+    }
+
+    /**
+     * 时间复杂度n^2有点大。
+     *
+     * @param c
+     * @return
+     */
     public boolean judgeSquareSum(int c)
     {
         boolean b = false;
@@ -17,8 +36,8 @@ public class SumOfSqareNumbers
         {
             for (int j = 0; j <= Math.sqrt(c); j++)
             {
-                int temp1 = (int)Math.pow(i, 2);
-                int temp2 = (int)Math.pow(j, 2);
+                int temp1 = (int) Math.pow(i, 2);
+                int temp2 = (int) Math.pow(j, 2);
                 if (temp1 + temp2 == c)
                 {
                     b = true;
@@ -30,10 +49,86 @@ public class SumOfSqareNumbers
         return b;
     }
 
-    public static void main(String[] args)
+    /**
+     * 时间复杂度n.
+     *
+     * @param c
+     * @return
+     */
+    public boolean judgeSquareSumOne(int c)
     {
-        boolean b = new SumOfSqareNumbers().judgeSquareSum(4);
-
-        System.out.println(b);
+        boolean b = false;
+        for (int i = (int) Math.sqrt(c); i >= 0; i--)
+        {
+            if (i * i == c)
+            {
+                b = true;
+                break;
+            }
+            int temp = c - i * i;
+            int t = (int) Math.sqrt(temp);
+            if (t * t == temp)
+            {
+                b = true;
+                break;
+            }
+        }
+        return b;
     }
+
+    /**
+     * 时间复杂度也是n
+     *
+     * @param c
+     * @return
+     */
+    public boolean judgeSquareSumTwo(int c)
+    {
+        boolean b = false;
+        Set<Integer> list = new HashSet<>();
+        for (int i = 0; i <= (int) Math.sqrt(c); i++)
+        {
+            list.add(i * i);
+            if (list.contains(c - i * i))
+            {
+                b = true;
+                break;
+            }
+        }
+
+        return b;
+
+    }
+
+    /**
+     * 时间复杂度也是n,效率最高。
+     * @param c
+     * @return
+     */
+    public boolean judgeSquareSumThree(int c)
+    {
+        boolean boo = false;
+        int a = 0;
+        int b = (int) Math.sqrt(c);
+        while (a <= b)
+        {
+            if (a * a + b * b == c)
+            {
+                boo = true;
+                break;
+            }
+            else if (a * a + b * b > c)
+            {
+                b--;
+            }
+            else
+            {
+                a++;
+            }
+        }
+
+        return boo;
+    }
+
+
 }
