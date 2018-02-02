aaf50a1076dc595ba3b8e61e24e4bf5851de7502
zky <1964483239@qq.com>
2018-02-02 23:57:43 +0800

    507. Perfect Number

---
+++ src/main/java/com/leetcode/PerfectNumber507/PerfectNumber.java
@@ -0,0 +1,26 @@
+package com.leetcode.PerfectNumber507;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-02
+ * Time: 23:56:18
+ * Description:
+ * <p>
+ * We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except
+ * itself.
+ * <p>
+ * Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+ * Example:
+ * Input: 28
+ * Output: True
+ * Explanation: 28 = 1 + 2 + 4 + 7 + 14
+ * Note: The input number n will not exceed 100,000,000. (1e8)
+ */
+public class PerfectNumber
+{
+    public boolean checkPerfectNumber(int num)
+    {
+
+    }
+}
