afbcbedc9481a26f97d406c2095ae7ccfea5b9aa
zky <1964483239@qq.com>
2018-02-13 21:37:51 +0800

    35. Search Insert Position

---
+++ src/main/java/com/leetcode/SearchInsertPosition35/README.md
@@ -0,0 +1,4 @@
+#Explain
+1. O(n) and it is good to improve your skill.
+2. I like this subject and I like to solve this kind of subject.
+3. Love your family and your folks chang their life in the world.
---
+++ src/test/java/com/leetcode/SearchInsertPosition35/SearchInsertPositionTest.java
@@ -0,0 +1,43 @@
+package com.leetcode.SearchInsertPosition35;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-13
+ * Time: 21:16:11
+ * Description:
+ */
+class SearchInsertPositionTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        System.out.println("String...");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        System.out.println("Ending...");
+    }
+
+    @Test
+    void searchInsert()
+    {
+        SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+
+        int [] nums = {1,3,5,6};
+        int [] nums1 = {1,3,5,6};
+        int i = searchInsertPosition.searchInsert(nums, 2);
+        int i1 = searchInsertPosition.searchInsert(nums1, 5);
+
+
+        System.out.println(i);
+        System.out.println(i1);
+    }
+}
