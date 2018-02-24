32c042d59251624550d02305dc46933f06d9f669
zky <1964483239@qq.com>
2018-02-24 22:18:24 +0800

    21. Merge Two Sorted Lists

---
+++ src/main/java/com/leetcode/MergeTwoSortedLists21/ListNode.java
@@ -0,0 +1,19 @@
+package com.leetcode.MergeTwoSortedLists21;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-24
+ * Time: 21:53:24
+ * Description:
+ */
+public class ListNode
+{
+    int val;
+    ListNode next;
+
+    ListNode(int x)
+    {
+        val = x;
+    }
+}
---
+++ src/test/java/com/leetcode/MergeTwoSortedLists21/MergeTwoSortedListsTest.java
@@ -0,0 +1,49 @@
+package com.leetcode.MergeTwoSortedLists21;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-24
+ * Time: 22:04:36
+ * Description:
+ */
+class MergeTwoSortedListsTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        System.out.println("String...");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        System.out.println("Ending...");
+    }
+
+    @Test
+    void mergeTwoLists()
+    {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        node1.next = new ListNode(4);
+        node1.next.next = new ListNode(5);
+
+        ListNode node2 = mergeTwoSortedLists.mergeTwoLists(node, node1);
+
+        while(node2 != null)
+        {
+            System.out.println(node2.val);
+            node2 = node2.next;
+        }
+    }
+}
