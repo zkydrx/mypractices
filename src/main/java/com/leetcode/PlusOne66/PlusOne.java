db2550d0f27d812d80e5d7a1ea6a372776554523
zky <1964483239@qq.com>
2018-02-28 23:56:54 +0800

    66. Plus One

---
+++ src/main/java/com/leetcode/PlusOne66/PlusOne.java
@@ -0,0 +1,23 @@
+package com.leetcode.PlusOne66;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-28
+ * Time: 23:55:58
+ * Description:
+ * 66. Plus One
+ * <p>
+ * Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+ * <p>
+ * You may assume the integer do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * The digits are stored such that the most significant digit is at the head of the list.
+ */
+public class PlusOne
+{
+    public int[] plusOne(int[] digits)
+    {
+
+    }
+}
