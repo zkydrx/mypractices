96b1bfc8a38aff7f5b74b5a6420328c14628ae37
zky <1964483239@qq.com>
2018-02-28 09:35:02 +0800

    53. Maximum Subarray

---
+++ src/main/java/com/leetcode/MaximumSubarray53/README.md
@@ -0,0 +1,4 @@
+#Explain
+1. The answer from the internet and it webist is : https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+2. I think this way was clever and intricate
+3. Arithmetic is very useful and I will  study more knowledgeable for it.
---
+++ src/test/java/com/leetcode/MaximumSubarray53/MaximumSubarrayTest.java
@@ -0,0 +1,39 @@
+package com.leetcode.MaximumSubarray53;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-02-28
+ * Time: 08:59:32
+ * Description:
+ */
+class MaximumSubarrayTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        System.out.println("String...");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        System.out.println("Ending...");
+    }
+
+    @Test
+    void maxSubArray()
+    {
+        MaximumSubarray maximumSubarray = new MaximumSubarray();
+
+        int [] nums= {-2, -3, 4, -1, -2, 1, 5, -3};
+        int i = maximumSubarray.maxSubArray(nums);
+
+        System.out.println(i);
+    }
+}
