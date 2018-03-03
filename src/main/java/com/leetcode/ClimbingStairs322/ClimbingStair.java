dd31d922aa8d2cd985e9ca67ea4377d0ec79a97e
zky <1964483239@qq.com>
2018-03-03 22:50:56 +0800

    70. Climbing Stairs

---
+++ src/main/java/com/leetcode/ClimbingStairs322/ClimbingStair.java
@@ -0,0 +1,45 @@
+package com.leetcode.ClimbingStairs322;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-02
+ * Time: 15:58:30
+ * Description:
+ * <p>
+ * 322. Climbing Stairs
+ * <p>
+ * <p>
+ * <p>
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * Note: Given n will be a positive integer.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 2
+ * Output:  2
+ * Explanation:  There are two ways to climb to the top.
+ * <p>
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ * <p>
+ * Input: 3
+ * Output:  3
+ * Explanation:  There are three ways to climb to the top.
+ * <p>
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ */
+public class ClimbingStair
+{
+    public int climbStairs(int n)
+    {
+        return 0;
+    }
+}
