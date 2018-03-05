3daaf44951ec20386a693ed90d4e21455769dddd
zky <1964483239@qq.com>
2018-03-05 23:41:11 +0800

    83. Remove Duplicates from Sorted List

---
+++ src/main/java/com/leetcode/RemoveDuplicatesFromSortedList83/ListNode.java
@@ -0,0 +1,15 @@
+package com.leetcode.RemoveDuplicatesFromSortedList83;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-05
+ * Time: 23:33:12
+ * Description:
+ */
+public class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(int x){val =x;}
+}
