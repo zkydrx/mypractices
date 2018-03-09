20291a0b912d35ab1ef7aae7f1a29b72e0cc2fee
zky <1964483239@qq.com>
2018-03-09 09:22:24 +0800

    101. Symmetric Tree

---
+++ src/test/java/com/leetcode/SymmetricTree101/SymmertricTreeTest.java
@@ -0,0 +1,45 @@
+package com.leetcode.SymmetricTree101;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-09
+ * Time: 09:15:19
+ * Description:
+ */
+class SymmertricTreeTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        System.out.println("String...");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        System.out.println("Ending...");
+    }
+
+    @Test
+    void isSymmetric()
+    {
+        SymmertricTree symmertricTree = new SymmertricTree();
+
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(2);
+        treeNode.left.left = new TreeNode(3);
+        treeNode.left.right = new TreeNode(4);
+        treeNode.right.left = new TreeNode(4);
+        treeNode.right.right = new TreeNode(3);
+        boolean symmetric = symmertricTree.isSymmetric(treeNode);
+
+        System.out.println(symmetric);
+    }
+}
