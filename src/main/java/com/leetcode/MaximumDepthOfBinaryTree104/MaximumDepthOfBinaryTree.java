478783bd5881f7f7975b7f7bba1e39daec151318
zky <1964483239@qq.com>
2018-03-10 23:38:20 +0800

    104. Maximum Depth of Binary Tree

---
+++ src/main/java/com/leetcode/MaximumDepthOfBinaryTree104/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+package com.leetcode.MaximumDepthOfBinaryTree104;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-10
+ * Time: 23:33:43
+ * Description:
+ *
+ * 104. Maximum Depth of Binary Tree
+ * <p>
+ * Given a binary tree, find its maximum depth.
+ * <p>
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its depth = 3.
+ */
+public class MaximumDepthOfBinaryTree
+{
+    public int maxDepth(TreeNode root)
+    {
+        return 0;
+    }
+}
---
+++ src/main/java/com/leetcode/MaximumDepthOfBinaryTree104/TreeNode.java
@@ -0,0 +1,20 @@
+package com.leetcode.MaximumDepthOfBinaryTree104;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-10
+ * Time: 23:36:21
+ * Description:
+ */
+public class TreeNode
+{
+    int val;
+    com.leetcode.SymmetricTree101.TreeNode left;
+    com.leetcode.SymmetricTree101.TreeNode right;
+
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
