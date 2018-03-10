4fe352e2088ae0c7da4d7df15f7c5b6c2f495a6a
zky <1964483239@qq.com>
2018-03-11 00:57:57 +0800

    107. Binary Tree Level Order Traversal II

---
+++ src/main/java/com/leetcode/BinaryTreeLevelOrderTraversalII107/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,40 @@
+package com.leetcode.BinaryTreeLevelOrderTraversalII107;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-11
+ * Time: 00:55:21
+ * Description:
+ * <p>
+ * 107. Binary Tree Level Order Traversal II
+ * <p>
+ * <p>
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right,
+ * level by level from leaf to root).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its bottom-up level order traversal as:
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+public class BinaryTreeLevelOrderTraversalII
+{
+    public List<List<Integer>> levelOrderBottom(TreeNode root)
+    {
+        List<List<Integer>> list = new ArrayList<>();
+        return list;
+    }
+}
