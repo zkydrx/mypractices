596eb4debd83e88b27667d6da2593d81bc5396b9
zky <1964483239@qq.com>
2018-03-16 11:15:45 +0800

    110. Balanced Binary Tree

---
+++ src/main/java/com/leetcode/MinimumDepthOfBinaryTree111/MiniMumDepthOfBinarytree.java
@@ -0,0 +1,21 @@
+package com.leetcode.MinimumDepthOfBinaryTree111;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-16
+ * Time: 11:14:14
+ * Description:
+ * <p>
+ * 111. Minimum Depth of Binary Tree
+ * Given a binary tree, find its minimum depth.
+ * <p>
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ */
+public class MiniMumDepthOfBinarytree
+{
+    public int minDepth(TreeNode root)
+    {
+        return 0;
+    }
+}
---
+++ src/main/java/com/leetcode/MinimumDepthOfBinaryTree111/TreeNode.java
@@ -0,0 +1,19 @@
+package com.leetcode.MinimumDepthOfBinaryTree111;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-16
+ * Time: 11:14:52
+ * Description:
+ */
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode (int x)
+    {
+        val = x;
+    }
+}
