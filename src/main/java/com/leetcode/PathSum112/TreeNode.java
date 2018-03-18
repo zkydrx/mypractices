d5e54be3ea549201a569abca5c2d3e00b7e2254f
zky <1964483239@qq.com>
2018-03-18 23:26:01 +0800

    112. Path Sum

---
+++ src/main/java/com/leetcode/PathSum112/TreeNode.java
@@ -0,0 +1,19 @@
+package com.leetcode.PathSum112;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-18
+ * Time: 23:24:36
+ * Description:
+ */
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
