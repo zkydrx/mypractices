e94f6b44198ed4b518d7f4f6b5df6554db46e5b2
zky <1964483239@qq.com>
2018-03-20 09:10:38 +0800

    121. Best Time to Buy and Sell Stock

---
+++ src/main/java/com/leetcode/BestTimeToBuyAndSellStock121/README.md
@@ -0,0 +1,3 @@
+#Explain
+1. Instead of keeping track of largest element in the array, we track the maximum profit so far.
+2. Design pattern is should to study hard and practice a lot and think more.
---
+++ src/test/java/com/leetcode/BestTimeToBuyAndSellStock121/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,39 @@
+package com.leetcode.BestTimeToBuyAndSellStock121;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-20
+ * Time: 08:59:23
+ * Description:
+ */
+class BestTimeToBuyAndSellStockTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        System.out.println("String...");
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        System.out.println("Ending...");
+    }
+
+    @Test
+    void maxProfit()
+    {
+        BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+
+        int[] nums={7, 6, 4, 3, 1};
+        int i = bestTimeToBuyAndSellStock.maxProfit(nums);
+
+        System.out.println(i);
+    }
+}
