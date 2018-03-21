66c3486a0f1ed853bcee74e40fafb62657c7f61f
zky <1964483239@qq.com>
2018-03-21 13:42:56 +0800

    Intercepting Filter

---
+++ src/main/java/KnowledgeableReview201803/designPatterns/InterceptingFilter/DebugFilter.java
@@ -0,0 +1,18 @@
+package KnowledgeableReview201803.designPatterns.InterceptingFilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-21
+ * Time: 13:28:50
+ * Description:
+ */
+public class DebugFilter implements Filter
+{
+
+    @Override
+    public void execute(String request)
+    {
+        System.out.println("request log: "+request);
+    }
+}
---
+++ src/main/java/KnowledgeableReview201803/designPatterns/InterceptingFilter/FilterManager.java
@@ -0,0 +1,29 @@
+package KnowledgeableReview201803.designPatterns.InterceptingFilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-21
+ * Time: 13:33:05
+ * Description:
+ */
+public class FilterManager
+{
+    FilterChain filterChain;
+    public FilterManager(Target target)
+    {
+        filterChain = new FilterChain();
+        filterChain.setTarget(target);
+    }
+
+    public void setFilter(Filter filter)
+    {
+        filterChain.addFilter(filter);
+    }
+
+    public void filterRequest(String request)
+    {
+        filterChain.execute(request);
+    }
+
+}
