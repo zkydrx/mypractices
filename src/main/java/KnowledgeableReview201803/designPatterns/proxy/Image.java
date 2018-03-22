c705e78bec7620a0e7baf63dd6861a59304e8bd7
zky <1964483239@qq.com>
2018-03-22 10:21:51 +0800

    Proxy Pattern

---
+++ src/main/java/KnowledgeableReview201803/designPatterns/proxy/Image.java
@@ -0,0 +1,13 @@
+package KnowledgeableReview201803.designPatterns.proxy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-22
+ * Time: 09:28:30
+ * Description:
+ */
+public interface Image
+{
+    void display();
+}
---
+++ src/main/java/KnowledgeableReview201803/designPatterns/proxy/Test.java
@@ -0,0 +1,17 @@
+package KnowledgeableReview201803.designPatterns.proxy;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-22
+ * Time: 09:34:49
+ * Description:
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Image image = new ProxyImage("BeautyGirl.jpg");
+        image.display();
+    }
+}
