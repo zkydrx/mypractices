fdae114b14f60b8702d4482a8d890fd44907f7dc
zky <1964483239@qq.com>
2018-03-24 22:16:37 +0800

    155. Min Stack

---
+++ src/main/java/com/leetcode/MinStack155/MinStack.java
@@ -0,0 +1,57 @@
+package com.leetcode.MinStack155;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-24
+ * Time: 22:11:58
+ * Description:
+ * 155. Min Stack
+ * <p>
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * Example:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ */
+public class MinStack
+{
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack()
+    {
+
+    }
+
+    public void push(int x)
+    {
+
+    }
+
+    public void pop()
+    {
+
+    }
+
+    public int top()
+    {
+        return 0;
+    }
+
+    public int getMin()
+    {
+        return 0;
+    }
+
+}
