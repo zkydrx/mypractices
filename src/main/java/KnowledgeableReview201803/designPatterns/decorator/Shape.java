56d138178c46180c534584553ba3a59133853dbd
zky <1964483239@qq.com>
2018-03-27 00:41:49 +0800

    Decorator Pattern

---
+++ src/main/java/KnowledgeableReview201803/designPatterns/decorator/Shape.java
@@ -0,0 +1,13 @@
+package KnowledgeableReview201803.designPatterns.decorator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Abbot
+ * Date: 2018-03-27
+ * Time: 00:26:44
+ * Description:
+ */
+public interface Shape
+{
+    void draw();
+}
