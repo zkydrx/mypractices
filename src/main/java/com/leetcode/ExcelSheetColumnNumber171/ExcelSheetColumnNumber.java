9b0a8e048791ff800ff6a037c696d0daa39a933d
zky <zkydrx@gmail.com>
2018-04-01 23:43:12 +0800

    171. Excel Sheet Column Number

---
+++ src/main/java/com/leetcode/ExcelSheetColumnNumber171/ExcelSheetColumnNumber.java
@@ -0,0 +1,31 @@
+package com.leetcode.ExcelSheetColumnNumber171;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zky
+ * Date: 2018-03-31
+ * Time: 22:02:13
+ * Description:
+ * Related to question Excel Sheet Column Title
+ * <p>
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * <p>
+ * For example:
+ * <p>
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * Credits:
+ * Special thanks to @ts for adding this problem and creating all test cases.
+ */
+public class ExcelSheetColumnNumber
+{
+    public int titleToNumber(String s)
+    {
+        return 0;
+    }
+}
