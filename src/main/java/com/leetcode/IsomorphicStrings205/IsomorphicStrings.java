2445079e51567a7a83845c14e819b761bc01520d
zky <1964483239@qq.com>
2018-04-21 23:41:25 +0800

    205. Isomorphic Strings

---
+++ src/main/java/com/leetcode/IsomorphicStrings205/IsomorphicStrings.java
@@ -0,0 +1,36 @@
+package com.leetcode.IsomorphicStrings205;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-04-21
+ * Time: 23:39:51
+ * Description:
+ * <p>
+ * 205. Isomorphic Strings
+ * <p>
+ * <p>
+ * Given two strings s and t, determine if they are isomorphic.
+ * <p>
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ * <p>
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character but a character may map to itself.
+ * <p>
+ * For example,
+ * Given "egg", "add", return true.
+ * <p>
+ * Given "foo", "bar", return false.
+ * <p>
+ * Given "paper", "title", return true.
+ * <p>
+ * Note:
+ * You may assume both s and t have the same length.
+ */
+public class IsomorphicStrings
+{
+    public boolean isIsomorphic(String s, String t)
+    {
+        return false;
+    }
+}
