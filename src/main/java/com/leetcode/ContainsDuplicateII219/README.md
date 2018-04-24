87327446d93e3cf381b812fabd1e420523de5a1e
zky <1964483239@qq.com>
2018-04-24 21:59:47 +0800

    219. Contains Duplicate II

---
+++ src/main/java/com/leetcode/ContainsDuplicateII219/README.md
@@ -0,0 +1,13 @@
+#Explain
+1. It is amazing.
+2. It use the HashMap's method and exchange the key and value.
+    
+    
+    if (map.containsKey(nums[i]))
+    {
+        int pre = map.get(nums[i]);
+        if (i - pre <= k)
+        {
+            return true;
+        }
+    }
---
+++ src/main/java/test/TestStringTrim.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-04-24
+ * Time: 11:25:26
+ * Description:
+ */
+public class TestStringTrim
+{
+    @Test
+    public void testStringTrim()
+    {
+        String trim = "12345".trim();
+
+        System.out.println(trim);
+    }
+}
