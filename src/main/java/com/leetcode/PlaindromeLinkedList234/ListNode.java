032aec29cf56a27cf0433b3d4711e92776a41eed
zky <1964483239@qq.com>
2018-04-28 23:54:30 +0800

    234. Palindrome Linked List

---
+++ src/main/java/com/leetcode/PlaindromeLinkedList234/ListNode.java
@@ -0,0 +1,18 @@
+package com.leetcode.PlaindromeLinkedList234;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-04-28
+ * Time: 23:23:07
+ * Description:
+ */
+public class ListNode
+{
+    int val;
+    ListNode next;
+    ListNode(int x)
+    {
+        val =x;
+    }
+}
---
+++ src/main/java/com/leetcode/PlaindromeLinkedList234/PlaindromeLinkedList.java
@@ -0,0 +1,23 @@
+package com.leetcode.PlaindromeLinkedList234;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-04-28
+ * Time: 23:21:46
+ * Description:
+ * <p>
+ * 234. Palindrome Linked List
+ * <p>
+ * Given a singly linked list, determine if it is a palindrome.
+ * <p>
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ */
+public class PlaindromeLinkedList
+{
+    public boolean isPalindrome(ListNode head)
+    {
+        return false;
+    }
+}
