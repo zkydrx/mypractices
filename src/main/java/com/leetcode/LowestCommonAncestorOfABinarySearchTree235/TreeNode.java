b8e4b61d71d6200c4e23650213f0b7fbcb1143c8
zky <1964483239@qq.com>
2018-05-01 23:47:16 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/com/leetcode/LowestCommonAncestorOfABinarySearchTree235/TreeNode.java
@@ -0,0 +1,19 @@
+package com.leetcode.LowestCommonAncestorOfABinarySearchTree235;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-05-01
+ * Time: 23:46:17
+ * Description:
+ */
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x)
+    {
+        val = x;
+    }
+}
