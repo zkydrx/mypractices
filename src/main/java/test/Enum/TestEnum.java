044d73ca9134c09ae511091edad8f804628587c9
zky <1964483239@qq.com>
2018-07-06 20:12:43 +0800

    enum test.

---
+++ src/main/java/test/Enum/TestEnum.java
@@ -0,0 +1,17 @@
+package test.Enum;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-07-06
+ * Time: 19:28:10
+ * Description:
+ */
+public class TestEnum
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println(AEnum.newInstance("C").getCode());
+    }
+}
