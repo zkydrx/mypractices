f09598e39d9787cda11173e0436b12fe3eef1791
zky <1964483239@qq.com>
2018-11-15 14:43:24 +0800

    Email test.

--- src/main/java/workTest/emailTest/EmailTest.java
+++ src/main/java/workTest/emailTest/EmailTest.java
@@ -1,14 +1,16 @@
 package workTest.emailTest;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeUtility;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.MultiPartEmail;
 import org.junit.jupiter.api.Test;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeUtility;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
@@ -21,6 +23,10 @@ import java.util.Properties;
  */
 public class EmailTest
 {
+    /**
+     * 基于apache的邮件框架仅支持jdk1.8及更高版本
+     * @param args
+     */
     public static void main(String[] args)
     {
         MultiPartEmail mail = new MultiPartEmail();
@@ -48,6 +54,9 @@ public class EmailTest
     }
 
 
+    /**
+     * 仅支持jdk1.8
+     */
     @Test
     public void testJavaMail()
     {
@@ -92,5 +101,65 @@ public class EmailTest
             e.printStackTrace();
         }
 
+    }
+
+
+    /**
+     * 使用springemail 的邮件工具完美解决TLS问题（javax.mail.MessagingException: Could not convert socket to TLS;）
+     * 同时支持jdk1.7和jdk1.8
+     */
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+    /**
+     * 初始化邮件发送数据
+     *
+     * @param host     服务器
+     * @param username 发送人
+     * @param passwd   发送人密码
+     */
+    public void initData(String host, String username, String passwd)
+    {
+        //创建邮件发送服务器
+        mailSender.setHost(host);
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(passwd);
+        //加认证机制
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.auth", true);
+        javaMailProperties.put("mail.smtp.starttls.enable", true);
+        javaMailProperties.put("mail.smtp.timeout", 5000);
+        javaMailProperties.put("mail.smtp.ssl.trust", "*");
+        mailSender.setJavaMailProperties(javaMailProperties);
+        System.out.println("初始化邮件发送信息完成");
+    }
+
+    /**
+     * 发送普通文本
+     *
+     * @param email   对方邮箱地址
+     * @param subject 主题
+     * @param text    邮件内容
+     */
+    public void simpleMailSend(String email, String subject, String text)
+    {
+        //创建邮件内容
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(mailSender.getUsername());
+        message.setTo(email);
+        message.setSubject(subject);
+        message.setText(text);
+        //发送邮件
+        mailSender.send(message);
+        System.out.println("发送成功");
+    }
+
+
+    @Test
+    public void testSpringEmail()
+    {
+        initData("****", "test@****.com", "****");
+
+        simpleMailSend("****@qq.com", "A", "123");
     }
 }
