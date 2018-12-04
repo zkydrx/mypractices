57bdd463c51f77b9fa72f25800bf370b5201268e
zkydrx <1964483239@qq.com>
2018-12-04 19:53:54 +0800

    bigdecimal test.

---
+++ src/main/java/workTest/Bigdecimal/BigdecimalTest.java
@@ -0,0 +1,20 @@
+package workTest.Bigdecimal;
+
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2018-12-04
+ * Time: 13:59:09
+ * Description:
+ */
+public class BigdecimalTest
+{
+    public static void main(String[] args)
+    {
+        BigDecimal bigDecimal = BigDecimal.valueOf(0.335234);
+        BigDecimal bigDecimal1 = bigDecimal.setScale(2,BigDecimal.ROUND_HALF_UP);
+        System.out.println(bigDecimal1);
+    }
+}
