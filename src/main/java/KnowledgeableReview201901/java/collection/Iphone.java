0abb7378c8b000ed854259ab7eeef2be42ee717d
zky <1964483239@qq.com>
2019-01-17 23:47:22 +0800

    Integer and int.

---
+++ src/main/java/KnowledgeableReview201901/java/collection/Iphone.java
@@ -0,0 +1,77 @@
+package KnowledgeableReview201901.java.collection;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2019-01-15
+ * Time: 22:04:10
+ * Description:
+ */
+public class Iphone implements Comparable<Iphone>,Serializable
+{
+    private static final long serialVersionUID = -2553740936632558776L;
+    private int id;
+    private int age;
+    private String name;
+
+    public Iphone()
+    {
+    }
+
+    public Iphone(int id, int age, String name)
+    {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Iphone{" + "id=" + id + ", age=" + age + ", name='" + name + '\'' + '}';
+    }
+
+    @Override
+    public int compareTo(Iphone o)
+    {
+        if(this.age-o.age==0)
+        {
+            return o.id-this.id;
+        }
+        return this.age-o.age;
+    }
+
+
+}
--- src/main/java/KnowledgeableReview201901/java/integer/TestInteger.java
+++ src/main/java/KnowledgeableReview201901/java/integer/TestInteger.java
@@ -25,4 +25,16 @@ public class TestInteger
             System.out.println("a != b他俩是两个对象");
         }
     }
+
+    /**
+     * Integer自动拆箱为int再去比
+     * (因为在Integer进行了拆箱之后：是int和int 相比；拆箱方法：intValue())
+     */
+    @Test
+    public void testIntAndInteger()
+    {
+        Integer a = 1;
+        int b = 1;
+        System.out.println(a==b);
+    }
 }
