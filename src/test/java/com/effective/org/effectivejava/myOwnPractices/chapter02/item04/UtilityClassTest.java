08af4d2bedac2e23f678e9f144356a9cd34e876a
zky <1964483239@qq.com>
2019-04-03 00:01:02 +0800

    Suppress default constructor for noninstantiability

---
+++ src/test/java/com/effective/org/effectivejava/myOwnPractices/chapter02/item04/UtilityClassTest.java
@@ -0,0 +1,16 @@
+package com.effective.org.effectivejava.myOwnPractices.chapter02.item04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2019-04-02
+ * Time: 23:56:01
+ * Description:
+ */
+class UtilityClassTest
+{
+    //不能被实例化！！！
+    // UtilityClass utilityClass = new UtilityClass();
+}
