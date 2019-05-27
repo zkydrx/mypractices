192b7c71380eede6c7df0bcd8163e826e769320a
zky <1964483239@qq.com>
2019-05-27 17:48:19 +0800

    multi thread.

---
+++ src/main/java/com/zhihei/multi/thread/TraditionalThreadSynchronized.java
@@ -0,0 +1,72 @@
+package com.zhihei.multi.thread;
+
+/**
+ * 传统方式实现线程互斥
+ *
+ * @author Jerry Wang
+ */
+public class TraditionalThreadSynchronized
+{
+
+    public static void main(String[] args)
+    {
+        new TraditionalThreadSynchronized().init();
+    }
+
+    private void init()
+    {
+        final Outputer outputer = new Outputer();
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    outputer.output("xxxxxxxxxxxxxxxxxx");
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Thread.sleep(10);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    outputer.output("yyyyyyyyyyyyyyyyyy");
+                }
+            }
+        }).start();
+    }
+
+    class Outputer
+    {
+        public synchronized void output(String name)
+        {
+            int len = name.length();
+            for (int i = 0; i < len; i++)
+            {
+                System.out.print(name.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+}
