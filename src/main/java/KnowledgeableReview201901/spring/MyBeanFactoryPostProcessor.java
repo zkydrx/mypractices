d4e91b74cb7a0c1bb5476d9d35d0b6bfe0f88b9e
zky <zkydrx@gmail.com>
2019-11-17 17:09:59 +0800

    g1

--- src/main/java/KnowledgeableReview201901/spring/MyBeanFactoryPostProcessor.java
+++ src/main/java/KnowledgeableReview201901/spring/MyBeanFactoryPostProcessor.java
@@ -1,7 +1,6 @@
 package KnowledgeableReview201901.spring;
 
 
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -14,18 +13,19 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
  * Time: 10:37:29
  * Description:
  */
-public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor
+{
 
-    public MyBeanFactoryPostProcessor() {
+    public MyBeanFactoryPostProcessor()
+    {
         super();
         System.out.println("这是BeanFactoryPostProcessor实现类构造器！！");
     }
 
     @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
-            throws BeansException {
-        System.out
-                .println("BeanFactoryPostProcessor调用postProcessBeanFactory方法");
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0) throws BeansException
+    {
+        System.out.println("BeanFactoryPostProcessor调用postProcessBeanFactory方法");
         BeanDefinition bd = arg0.getBeanDefinition("person");
         bd.getPropertyValues().addPropertyValue("phone", "110");
     }
--- src/main/java/KnowledgeableReview201901/spring/MyInstantiationAwareBeanPostProcessor.java
+++ src/main/java/KnowledgeableReview201901/spring/MyInstantiationAwareBeanPostProcessor.java
@@ -6,6 +6,7 @@ import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 
 import java.beans.PropertyDescriptor;
+
 /**
  * Created with IntelliJ IDEA.
  * Author: zky
@@ -13,40 +14,36 @@ import java.beans.PropertyDescriptor;
  * Time: 10:36:37
  * Description:
  */
-public class MyInstantiationAwareBeanPostProcessor extends
-        InstantiationAwareBeanPostProcessorAdapter {
+public class MyInstantiationAwareBeanPostProcessor extends InstantiationAwareBeanPostProcessorAdapter
+{
 
-    public MyInstantiationAwareBeanPostProcessor() {
+    public MyInstantiationAwareBeanPostProcessor()
+    {
         super();
-        System.out
-                .println("这是InstantiationAwareBeanPostProcessorAdapter实现类构造器！！");
+        System.out.println("这是InstantiationAwareBeanPostProcessorAdapter实现类构造器！！");
     }
 
     // 接口方法、实例化Bean之前调用
     @Override
-    public Object postProcessBeforeInstantiation(Class beanClass,
-                                                 String beanName) throws BeansException {
-        System.out
-                .println("InstantiationAwareBeanPostProcessor调用postProcessBeforeInstantiation方法");
+    public Object postProcessBeforeInstantiation(Class beanClass, String beanName) throws BeansException
+    {
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessBeforeInstantiation方法");
         return null;
     }
 
     // 接口方法、实例化Bean之后调用
     @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName)
-            throws BeansException {
-        System.out
-                .println("InstantiationAwareBeanPostProcessor调用postProcessAfterInitialization方法");
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException
+    {
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessAfterInitialization方法");
         return bean;
     }
 
     // 接口方法、设置某个属性时调用
     @Override
-    public PropertyValues postProcessPropertyValues(PropertyValues pvs,
-                                                    PropertyDescriptor[] pds, Object bean, String beanName)
-            throws BeansException {
-        System.out
-                .println("InstantiationAwareBeanPostProcessor调用postProcessPropertyValues方法");
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs, PropertyDescriptor[] pds, Object bean, String beanName) throws BeansException
+    {
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessPropertyValues方法");
         return pvs;
     }
 }
