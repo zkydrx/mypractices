29134c67314b6a32ff559ed6aecd7620cdf06d03
zky <zkydrx@gmail.com>
2019-11-26 03:11:40 +0800

    blokcqueue.

--- src/main/java/com/zhihei/basicknowledge/queue/BlockingQueueTest.java
+++ src/main/java/com/zhihei/basicknowledge/queue/BlockingQueueTest.java
@@ -1,6 +1,11 @@
 package com.zhihei.basicknowledge.queue;
 
-import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,6 +13,9 @@ import java.util.List;
  * Date: 2019-11-23
  * Time: 23:09:21
  * Description:
+ * ArrayBlockingQueue:是一个基于数组结构的有界阻塞队列，此队列按FIFO(先进先出)原则对元素进行排序
+ * LinkedBlockingQUeue:一个基于链表结构的阻塞队列，此队列按FIFO(先进先出)排序元素，吞吐量通常要高于ArrayBlockingQueue
+ * SynchronousQueue:一个不存储元素的阻塞队列。每次插入数据必须要等到另外一个线程调用移除操作，否则插入操作一直处于阻塞状态
  * 1.队列
  * 2.阻塞队列
  * 2.1阻塞队列有没有好的一面
@@ -15,9 +23,159 @@ import java.util.List;
  */
 public class BlockingQueueTest
 {
-    public static void main(String[] args)
+    /**
+     * 越界报异常
+     */
+    @Test
+    public void testAddRemoveElement()
     {
-        List list = null;
+        BlockingQueue<String> blockingQueue= new ArrayBlockingQueue<>(2);
+        System.out.println(blockingQueue.add("1"));
+        System.out.println(blockingQueue.add("2"));
+        /**
+         * 返回队列头的元素，若头元素不存在则抛异常
+         * java.util.NoSuchElementException
+         */
+        System.out.println(blockingQueue.element());
+
+        /**
+         * 当队列满时再往队列里加新元素则会跑出异常
+         *  java.lang.IllegalStateException: Queue full
+         */
+        // System.out.println(blockingQueue.add("3"));
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        /**
+         * 当删除的元素超过队列长度时，将报异常：
+         * java.util.NoSuchElementException
+         */
+        // System.out.println(blockingQueue.remove());
+
+    }
+
+    /**
+     * 越界给出特殊值，
+     * offer->false
+     * poll->null
+     * peek->null
+     */
+    @Test
+    public void testOfferPollPeek()
+    {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        System.out.println(blockingQueue.offer("x"));
+        System.out.println(blockingQueue.offer("y"));
+        System.out.println(blockingQueue.offer("z"));
+        /**
+         * 超出队列长度返回false
+         */
+        System.out.println(blockingQueue.offer("!"));
+        /**
+         * 返回队首元素，若为空返回null
+         */
+        System.out.println(blockingQueue.peek());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        /**
+         * 超出队列长度以后返回null
+         */
+        System.out.println(blockingQueue.poll());
+
+
+    }
+
+    /**
+     * 阻塞
+     */
+    @Test
+    public void testPutTake()
+    {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        try
+        {
+            blockingQueue.put("a");
+            blockingQueue.put("b");
+            blockingQueue.put("c");
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            /**
+             * 如果放不下将一直阻塞
+             */
+            // blockingQueue.put("d");
+            System.out.println(blockingQueue.take());
+            System.out.println(blockingQueue.take());
+            System.out.println(blockingQueue.take());
+            /**
+             * 取不到也将一直阻塞
+             */
+            // System.out.println(blockingQueue.take());
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 超时退出
+     */
+    @Test
+    public void testOutOfTime() throws InterruptedException
+    {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+        System.out.println(blockingQueue.offer("a", 3, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("b", 3, TimeUnit.SECONDS));
+        System.out.println(blockingQueue.offer("c", 3, TimeUnit.SECONDS));
+        /**
+         * 超过设定的容量后，将会阻塞设定的时间，超过设定的时间，将自动退出
+         */
+        System.out.println(blockingQueue.offer("d", 3, TimeUnit.SECONDS));
+    }
+
+
+    /**
+     * SynchronousQueue没有容量
+     * 每一个put操作必须要等待以个take操作，否则不能继续添加元素，但是不阻塞
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSynchronousQueue() throws InterruptedException
+    {
+        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
+        new Thread(()->{
+            try
+            {
+                System.out.println(Thread.currentThread().getName()+"\t put 1");
+                blockingQueue.put("1");
+                System.out.println(Thread.currentThread().getName()+"\t put 2");
+                blockingQueue.put("2");
+                System.out.println(Thread.currentThread().getName()+"\t put 3");
+                blockingQueue.put("3");
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        },"A").start();
+
+
+        new Thread(()->{
+            try
+            {
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println(Thread.currentThread().getName()+"\t take"+blockingQueue.take());
+                // TimeUnit.SECONDS.sleep(5);
+                // System.out.println(Thread.currentThread().getName()+"\t take"+blockingQueue.take());
+                // TimeUnit.SECONDS.sleep(5);
+                // System.out.println(Thread.currentThread().getName()+"\t take"+blockingQueue.take());
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        },"B").start();
+        TimeUnit.SECONDS.sleep(20);
 
     }
 }
---
+++ src/main/java/com/zhihei/basicknowledge/queue/ProducerAndConsumerTraditionalTest.java
@@ -0,0 +1,349 @@
+package com.zhihei.basicknowledge.queue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2019-11-26
+ * Time: 01:11:18
+ * Description:
+ * 一个初始值为零的变量，两个线程对其交替操作，1个加1，1个减1，来10次
+ * 1.线程，操作,资源类
+ * 2.判读,干活，通知
+ * 3.防止虚假唤醒机制
+ * 来自于jdk8文档说明
+ * As in the one argument version, interrupts and spurious wakeups are possible, and this method should always be used in a loop:
+ *
+ *      synchronized (obj) {
+ *          while (<condition does not hold>)
+ *              obj.wait();
+ *          ... // Perform action appropriate to condition
+ *      }
+ */
+class DataClass //资源类
+{
+    private int number = 0;
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void increament()
+    {
+        lock.lock();
+        try
+        {
+            /**
+             * if判断会出现虚假唤醒，前提是大于两个线程的操作
+             * 如果只有两个线程用操作共享资源，用if无妨，如果大于两个线程
+             * 则要使用while来判断，否则会出现虚假唤醒。
+             * 数据出现了负值。6个线程操作资源类，出现了错误。
+             * 但是两个线程操作时用if判断是完全正确的。
+             * example:
+             * A	 current number: 1
+             * B	 current number: 0
+             * A	 current number: 1
+             * B	 current number: 0
+             * A	 current number: 1
+             * C	 current number: 2
+             * B	 current number: 1
+             * B	 current number: 0
+             * D	 current number: -1
+             * D	 current number: -2
+             * D	 current number: -3
+             * D	 current number: -4
+             * D	 current number: -5
+             * A	 current number: -4
+             * C	 current number: -3
+             * B	 current number: -4
+             * A	 current number: -3
+             * C	 current number: -2
+             * E	 current number: -1
+             * C	 current number: 0
+             * C	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * F	 current number: 0
+             */
+            // if (number == 0)
+            while (number != 0)
+            {
+                condition.await();
+            }
+            number++;
+            System.out.println(Thread.currentThread().getName() + "\t current number: " + number);
+            condition.signalAll();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+
+    public void decreament()
+    {
+        lock.lock();
+        try
+        {
+            /**
+             * if判断会出现虚假唤醒，前提是大于两个线程的操作
+             * 如果只有两个线程用操作共享资源，用if无妨，如果大于两个线程
+             * 则要使用while来判断，否则会出现虚假唤醒。
+             * 数据出现了负值。6个线程操作资源类，出现了错误。
+             * 但是两个线程操作时用if判断是完全正确的。
+             * example:
+             * A	 current number: 1
+             * B	 current number: 0
+             * A	 current number: 1
+             * B	 current number: 0
+             * A	 current number: 1
+             * C	 current number: 2
+             * B	 current number: 1
+             * B	 current number: 0
+             * D	 current number: -1
+             * D	 current number: -2
+             * D	 current number: -3
+             * D	 current number: -4
+             * D	 current number: -5
+             * A	 current number: -4
+             * C	 current number: -3
+             * B	 current number: -4
+             * A	 current number: -3
+             * C	 current number: -2
+             * E	 current number: -1
+             * C	 current number: 0
+             * C	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * E	 current number: 2
+             * F	 current number: 1
+             * F	 current number: 0
+             */
+            // if (number == 0)
+            while (number == 0)
+            {
+                condition.await();
+            }
+            number--;
+            System.out.println(Thread.currentThread().getName() + "\t current number: " + number);
+            condition.signalAll();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+
+}
+
+class DataSource
+{
+    private int number = 1;//1--A,2--B,3--C
+    private Lock lock = new ReentrantLock();
+    Condition condition1 = lock.newCondition();
+    Condition condition2 = lock.newCondition();
+    Condition condition3 = lock.newCondition();
+
+    public void printNumber5()
+    {
+        lock.lock();
+        try
+        {
+
+            while (number != 1)
+            {
+                try
+                {
+                    condition1.await();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 2;
+            condition2.signal();
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            lock.unlock();
+        }
+
+    }
+
+    public void printNumber10()
+    {
+        lock.lock();
+        try
+        {
+
+            while (number != 2)
+            {
+                try
+                {
+                    condition2.await();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 3;
+            condition3.signal();
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            lock.unlock();
+        }
+
+    }
+
+
+    public void printNumber15()
+    {
+        lock.lock();
+        try
+        {
+
+            while (number != 3)
+            {
+                try
+                {
+                    condition3.await();
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 1;
+            condition1.signal();
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            lock.unlock();
+        }
+
+    }
+
+}
+
+public class ProducerAndConsumerTraditionalTest
+{
+    @Test
+    public void testProducerAndConsumer() throws InterruptedException
+    {
+        DataClass dataClass = new DataClass();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.increament();
+            }
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.decreament();
+            }
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.increament();
+            }
+        }, "C").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.decreament();
+            }
+        }, "D").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.increament();
+            }
+        }, "E").start();
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++)
+            {
+                dataClass.decreament();
+            }
+        }, "F").start();
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+
+    /**
+     * 题目：
+     * 多线程按顺序调用，实现A,B,C三个线程自动启动，要求如下：
+     * A打印五次，B打印10次，C打印15次，循环10次。
+     *
+     * 多线程操作资源类
+     * 1.判断，
+     * 2.工作
+     * 3.唤醒注意虚假唤醒。（大于两个线程无法使用if进行唤醒，要使用while）
+     */
+    @Test
+    public void testLockAwaitSingal() throws InterruptedException
+    {
+        DataSource dataSource = new DataSource();
+        new Thread(dataSource::printNumber5, "a").start();
+        new Thread(dataSource::printNumber10, "b").start();
+        new Thread(dataSource::printNumber15, "c").start();
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+}
