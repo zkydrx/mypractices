3c692bf13405575c4252c8efb421c578a2a09783
zky <zkydrx@gmail.com>
2020-01-06 20:55:08 +0800

    将对象转成数组两种方法。

---
+++ src/main/java/com/tuku/utils/ArrayUtils.java
@@ -0,0 +1,53 @@
+package com.tuku.utils;
+
+import net.sf.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2020-01-06
+ * Time: 15:41:01
+ * Description:
+ */
+public class ArrayUtils
+{
+
+    /**
+     * 对象转数组
+     * 工具方式1
+     * net.sf.json
+     */
+    @Test
+    public void testToArray()
+    {
+        Person build = Person.builder().age("1").money(100.0).name("tom").sex('A').build();
+        /**
+         * 通过工具来将一个对象转成数组，全部是String数组
+         *
+         */
+        JSONArray jsonArray = JSONArray.fromObject(build);
+        for (Object o : jsonArray)
+        {
+            System.out.println(o);
+        }
+    }
+
+
+    /**
+     * 在实体类中添加toArray方法
+     */
+    @Test
+    public void testToArrayByOwnMethod()
+    {
+        Person build = Person.builder().sex('A').name("Tom").money(100.00).age("100").build();
+
+        String[] strings = build.toArray();
+
+        for (String string : strings)
+        {
+            System.out.println(string);
+        }
+    }
+
+}
---
+++ src/main/java/com/tuku/utils/Person.java
@@ -0,0 +1,27 @@
+package com.tuku.utils;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zky
+ * Date: 2020-01-06
+ * Time: 15:41:10
+ * Description:
+ */
+@Data
+@ToString
+@Builder
+public class Person
+{
+    private String name;
+    private String age;
+    private Double money;
+    private Character sex;
+    public String[] toArray()
+    {
+        return new String[]{name, age, String.valueOf(money), String.valueOf(sex)};
+    }
+}
