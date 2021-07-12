8624d100d1eb146140189265271e4fc24621d0cb
zky <zkydrx@gmail.com>
2021-07-12 10:30:56 +0800

    feat(method):
    map put vs =
    
    BREAKING CHANGE: work
    
    Closes no

--- src/main/java/com/hundsun/practices/map/MapAddMethod.java
+++ src/main/java/com/hundsun/practices/map/MapAddMethod.java
@@ -31,36 +31,37 @@ public class MapAddMethod
     @Test
     public void test1()
     {
-        Map<String, VarScrEntity> scrMap =new HashMap<>();
-        scrMap.put("a",VarScrEntity.builder().connectDpInterCode("a").dpInterCode("1").build());
-        scrMap.put("b",VarScrEntity.builder().connectDpInterCode("b").dpInterCode("2").build());
-        scrMap.put("c",VarScrEntity.builder().connectDpInterCode("c").dpInterCode("3").build());
-        log.info("put...{}",scrMap.toString());
+        Map<String, VarScrEntity> scrMap = new HashMap<>();
+        scrMap.put("a", VarScrEntity.builder().connectDpInterCode("a").dpInterCode("a").build());
+        scrMap.put("b", VarScrEntity.builder().connectDpInterCode("b").dpInterCode("b").build());
+        scrMap.put("c", VarScrEntity.builder().connectDpInterCode("c").dpInterCode("c").build());
+        log.info("put...{}", scrMap);
 
         Map<String, VarScrEntity> map = new HashMap<>();
-        map.put("a",VarScrEntity.builder().connectDpInterCode("a1").dpInterCode("11").build());
-        map.put("b",VarScrEntity.builder().connectDpInterCode("b2").dpInterCode("22").build());
-        map.put("c",VarScrEntity.builder().connectDpInterCode("c3").dpInterCode("33").build());
+        map.put("a",VarScrEntity.builder().connectDpInterCode("aa").dpInterCode("aa").build());
+        map.put("b",VarScrEntity.builder().connectDpInterCode("bb").dpInterCode("bb").build());
+        map.put("c",VarScrEntity.builder().connectDpInterCode("cc").dpInterCode("cc").build());
         scrMap.putAll(map);
-        log.info("putAll...{}",scrMap.toString());
+        log.info("putAll...{}", scrMap);
+        Map<String, VarScrEntity> map1 = new HashMap<>();
 
-        map.put("a",VarScrEntity.builder().connectDpInterCode("a11").dpInterCode("111").build());
-        map.put("b",VarScrEntity.builder().connectDpInterCode("b22").dpInterCode("222").build());
-        map.put("c",VarScrEntity.builder().connectDpInterCode("c33").dpInterCode("333").build());
-        scrMap=map;
-        log.info("==...{}",scrMap.toString());
-
-        map.put("d",VarScrEntity.builder().connectDpInterCode("d").dpInterCode("d").build());
-        map.put("e",VarScrEntity.builder().connectDpInterCode("e").dpInterCode("e").build());
-        map.put("f",VarScrEntity.builder().connectDpInterCode("f").dpInterCode("f").build());
-        scrMap.putAll(map);
-        log.info("key值不等的情况putAll...{}",scrMap.toString());
-
-        map.put("g",VarScrEntity.builder().connectDpInterCode("g").dpInterCode("g").build());
-        map.put("h",VarScrEntity.builder().connectDpInterCode("h").dpInterCode("h").build());
-        map.put("i",VarScrEntity.builder().connectDpInterCode("i").dpInterCode("i").build());
-        scrMap=map;
-        log.info("key值不等的情况赋值...{}",scrMap.toString());
+        map1.put("a",VarScrEntity.builder().connectDpInterCode("aaa").dpInterCode("aaa").build());
+        map1.put("b",VarScrEntity.builder().connectDpInterCode("bbb").dpInterCode("bbb").build());
+        map1.put("c",VarScrEntity.builder().connectDpInterCode("ccc").dpInterCode("ccc").build());
+        scrMap=map1;
+        log.info("==...{}", scrMap);
+        Map<String, VarScrEntity> map2 = new HashMap<>();
+        map2.put("d",VarScrEntity.builder().connectDpInterCode("d").dpInterCode("d").build());
+        map2.put("e",VarScrEntity.builder().connectDpInterCode("e").dpInterCode("e").build());
+        map2.put("f",VarScrEntity.builder().connectDpInterCode("f").dpInterCode("f").build());
+        scrMap.putAll(map2);
+        log.info("key值不等的情况putAll...{}", scrMap);
+        Map<String, VarScrEntity> map3 = new HashMap<>();
+        map3.put("g",VarScrEntity.builder().connectDpInterCode("g").dpInterCode("g").build());
+        map3.put("h",VarScrEntity.builder().connectDpInterCode("h").dpInterCode("h").build());
+        map3.put("i",VarScrEntity.builder().connectDpInterCode("i").dpInterCode("i").build());
+        scrMap=map3;
+        log.info("key值不等的情况赋值...{}", scrMap);
 
 
     }
